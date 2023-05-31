21cb4c6706edb5247de35e455920ec3d5d51da62
NItish Kumar <134680747+96nitishkumar@users.noreply.github.com>
2023-05-31 15:53:29 +0530

    Add files via upload

---
+++ AddTwoNumber/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NITISH KUMAR
+ *
+ */
+module AddTwoNumber {
+}
---
+++ Addition.java
@@ -0,0 +1,15 @@
+package com.add;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		
+	
+	int num1=30;
+	int num2=8;
+	int result;
+	result = num1*num2;
+	System.out.println("Multiplication of two number is :"+result);
+	}
+
+}
